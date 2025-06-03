4e5ecffdb1304d2ce1a4a451dfbdcf6aae573dd9
ubparviz <ubparviz@gmail.com>
2025-06-03 23:31:09 +0500

    Done

--- task06.py
+++ task06.py
@@ -5,13 +5,13 @@ number = int(input("Raqam kiriting: "))
 if number == 90 or number == 91:
     print("Ucell")
 
-if number == 93 or number == 94:
+elif number == 93 or number == 94:
     print("Beeline")
 
-if number == 95 or number == 97:
+elif number == 95 or number == 97:
     print("Uzmobile")
 
-if number == 88 or number == 99:
+elif number == 88 or number == 99:
     print("MobiUz")
 
 else:
